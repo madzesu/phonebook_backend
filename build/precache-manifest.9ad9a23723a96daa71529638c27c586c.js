5df14a8e08469343d1f9d2a2e2606cf6058013cb
madzesu <matias.ranta123@gmail.com>
2020-03-02 15:49:46 +0200

    uibuild

---
+++ build/precache-manifest.9ad9a23723a96daa71529638c27c586c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a832b4bc1242b5a88cad5d361fe849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1027b59a6e8ad630c96c",
+    "url": "/static/css/main.7d533e21.chunk.css"
+  },
+  {
+    "revision": "8bc6690c2a1287e0c347",
+    "url": "/static/js/2.666c895a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.666c895a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1027b59a6e8ad630c96c",
+    "url": "/static/js/main.c6599fa9.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static('build'));
+
+morgan.token('post-req-body', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body);
+    }
+    return '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-req-body'));
+
+
+let persons = [
+    {
+        name: 'arto hellas',
+        number: '61231231',
+        id: 1
+    },
+    {
+        name: 'Ada Lovelace',
+        number: '39-44-5323523',
+        id: 2
+    },
+    {
+        name: 'Dan Abramov',
+        number: '12-43-234345',
+        id: 3
+    },
+    {
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122',
+        id: 4
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(p => p.id === id);
+    if (person) {
+        return res.json(person);
+    }
+    res.status(404).end();
+});
+
+const generateId = (max = 10000000) => {
+    return Math.floor(Math.random() * Math.floor(max));
+};
+
+const personExists = name =>
+    persons.find(p => p.name.toLowerCase() === name.toLowerCase());
+
+const validatePersonPost = req => {
+    const body = req.body;
+
+    const fields = ['name', 'number'];
+    const errors = fields.reduce((currentErrors, field) => {
+        if (!body[field]) {
+            return {
+                ...currentErrors,
+                [field]: 'required field'
+            }
+        }
+        if (field === 'name' && personExists(body.name)) {
+            return {
+                ...currentErrors,
+                [field]: 'name must be unique'
+            };
+        }
+        return currentErrors;
+    }, {});
+
+    console.log('errors:', errors)
+
+    return errors;
+};
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+
+    const errors = validatePersonPost(req);
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json({ errors });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    };
+
+    persons = persons.concat(person);
+
+    res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    persons = persons.filter(p => p.id !== id);
+
+    res.status(204).end();
+});
+
+app.get('/info', (req, res) => {
+    const infoContent = `
+        <div>
+            <div>Phonebook has info for ${persons.length} people</div>
+            <div>${new Date()}</div>
+        </div>
+    `;
+    res.send(infoContent);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`server started and is listening port ${PORT}`);
+});
